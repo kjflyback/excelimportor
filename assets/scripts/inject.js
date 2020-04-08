070451e9ca934667667ff2438dd644af487523e1
kjflyback <tangxb@rising.com.cn>
2020-04-08 13:43:05 +0800

    add analyze for <select>

--- assets/scripts/inject.js
+++ assets/scripts/inject.js
@@ -12,7 +12,7 @@
                 port.postMessage({cmd:'url', url:window.location.href});
             }else if(msg.cmd == 'analyze' && msg.url == window.location.href){
                 var idList = [];
-                document.querySelectorAll('form input').forEach(function(obj){
+                document.querySelectorAll('form [name]').forEach(function(obj){
                     idList.push(obj.name);
                 });
                 port.postMessage({cmd:'analyze', namelist:idList,url:window.location.href});
@@ -45,9 +45,14 @@
 
             } else if (msg.cmd == 'filldata' && msg.url == window.location.href) {
                 //console.log(msg);
-                var obj = $("input[name='" + msg.id + "']");
-                //console.log(obj);
-                obj.val(msg.value);
+                var xpath = "[name='" + msg.id + "']";
+                // console.log(xpath);
+                var obj = document.querySelector(xpath);
+                var realVal = msg.value;
+                if(obj.type == "select-one"){
+                    realVal = $(obj).find("option:contains('" + msg.value + "')").val() || msg.value;
+                }
+                $(obj).val(realVal);
             } 
         });
         
@@ -58,15 +63,10 @@
     });
     function addFlowControl(row){
 
-    // }
-    // 
-
-        // if(response.url != window.location.href) return;
-
         var div = document.createElement("div");
         div.id = "flow";
-        div.innerHTML = "批量数据填表助手 行号:<span id='agency_row'></span><button id='agency_prev'>上一条</button><button id='agency_next'>下一条</button>";
-        div.style = "position:fixed;bottom: 1rem;left: 1rem;z-index: 9999;background-color: aqua;opacity:0.6;"
+        div.innerHTML = "批量数据填表助手 行号:<span id='agency_row'></span><br><button id='agency_prev'>上一条</button><button id='agency_next'>下一条</button>";
+        div.style = "box-shadow: 2px 2px 5px #888888;color:white;padding:10px;valign:middle;border-radius: 6px;position:fixed;bottom: 1rem;left: 1rem;z-index: 9999;background-color: darkblue;"
         document.body.appendChild(div);
 
         $("#agency_row").text(row);
