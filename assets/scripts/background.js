44e99ff70e2d9e69bf2f9c906da55c394a36f06f
kjflyback <tangxb@rising.com.cn>
2020-04-25 22:42:50 +0800

    reconstruct

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -1,234 +1,187 @@
-
-var port;
-var getUrlsCallBack = function(url){
-
-};
-var namelistCallBack;
-var cacheurls = [];
-var cacheData = {
-	workbook:undefined,
-	matchtable:[],
-	currentRow:0
-};
-
-function getCurrentTab(callback) {
-    chrome.tabs.query({
-        active: true
-    }, function (tabs) {
-        if (tabs && tabs[0]) {
-            callback(tabs[0]);
-        }
-    });
-}
-
-chrome.runtime.onMessage.addListener(function (request,sender,callback) {
-	
-	if(request.cmd == 'active'){
-		callback({cmd:'active', url:cacheData.url, row:cacheData.currentRow});
-		console.log(request);
-		getCurrentTab(function(tab){
-			if(port){
-				port.disconnect();
-				// port = undefined;
+var server = {
+	targetid: 0,
+	lock: false,
+	targettitle: "",
+	current: {
+		tabid: 0,
+		tabtitle: ""
+	},
+	saveTarget: function(id, title) {
+		// 保存当前的数据
+		this.current.tabid = id;
+		this.current.tabtitle = title;
+		if (this.lock) {
+			// 已经锁定则自动调用active
+			chrome.tabs.sendMessage(this.targetid, {
+				cmd: 'active'
+			});
+			return;
+		}
+		this.targetid = id;
+		this.targettitle = title;
+	},
+	urls: [],
+	init: function() {
+		if (this.lock) return;
+		this.urls = [];
+		this.match = {};
+	},
+	binding: function(urlcallback) {
+		// console.log(this.urls);
+		this.ucallback = urlcallback;
+		if (this.urls.length > 0) {
+			for (var k in this.urls) {
+				this.ucallback(this.urls[k].tabid, this.urls[k].href);
 			}
-	
-			port = chrome.tabs.connect(tab.id, {});
-	
-			port.onMessage.addListener(function (msg) {
-				// console.log('background.js' + msg);
-				if (msg.cmd == "url") {
-					cacheurls.push(msg.url);
-					if (getUrlsCallBack) {
-						getUrlsCallBack(msg.url);
-					}
-				}else if(msg.cmd == 'analyze'){
-					if (namelistCallBack) {
-						namelistCallBack(msg.namelist);
-					}
-				}
+			return;
+		}
+		chrome.tabs.query({
+			active: true
+		}, function(tabs) {
+			this.targetid = tabs[0].id;
+			chrome.tabs.sendMessage(this.targetid, {
+				cmd: 'query'
 			});
 		});
-	}else if(request.cmd == 'prev' &&request.url == cacheData.url){
-		if(cacheData.currentRow > 1)
-			cacheData.currentRow --;
-		for(var col in cacheData.matchtable){
-			var xlsCol = cacheData.matchtable[col];
-			// console.log(xlsCol);
-			var value = getXlsCell(xlsCol);
-			// console.log(value);
-			fillData(col, value);
-		}
-		callback({cmd:'prev', row:cacheData.currentRow});
-	}else if(request.cmd == 'next'&&request.url == cacheData.url){
-		cacheData.currentRow ++;
-		for(var col in cacheData.matchtable){
-			var xlsCol = cacheData.matchtable[col];
-			// console.log(xlsCol);
-			var value = getXlsCell(xlsCol);
-			// console.log(value);
-			fillData(col, value);
-		}
-		callback({cmd:'prev', row:cacheData.currentRow});
+	},
+	ucallback: undefined,
+	lockTarget: function(lock) {
+		// console.log(lock);
+		this.lock = lock;
+		// active inject page
+		chrome.tabs.sendMessage(this.targetid, {
+			cmd: 'active'
+		});
+	},
+	workbook: undefined,
+	sheet: undefined,
+	filename: undefined,
+	row: 0,
+	match:{}
+};
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	// console.log(tab);
+	if (tab.status != "complete") return;
+	var url = tab.url;
+	// console.log(tab);
+	if (url == undefined) return;
+
+	// save current title
+	server.saveTarget(tabId, tab.title);
+
+	try {
+		chrome.tabs.sendMessage(
+			tabId, {
+				cmd: 'query'
+			},
+			function(response) {
+				// console.log(response);
+			});
+	} catch (err) {
+		console.log(err);
+	}
+
+});
+
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	if (activeInfo.tabId) {
+		chrome.tabs.get(activeInfo.tabId, function(tab) {
+			// console.log(tab);
+			server.saveTarget(tab.id, tab.title);
+			try {
+				chrome.tabs.sendMessage(
+					tab.id, {
+						cmd: 'query'
+					},
+					function(response) {
+						// console.log(response);
+					});
+			} catch (err) {
+				console.log(err);
+			}
+		});
 	}
 });
 
-/*
-chrome.tabs.onUpdated.addListener(function(tabid, changinfo, tab){
-	console.log(changinfo);
-	if(changinfo.status != "complete") return;
-	getCurrentTab(function(tab){
-		console.log(tab.id);
-		port = chrome.tabs.connect(tab.id, {});
-
-		port.onMessage.addListener(function (msg) {
-			console.log(msg);
-			if (msg.cmd == "url") {
-				cacheurls.push(msg.url);
-				if (getUrlsCallBack) {
-					getUrlsCallBack(msg.url);
-				}
-			}else if(msg.cmd == 'analyze'){
-				if (namelistCallBack) {
-					namelistCallBack(msg.namelist);
-				}
-			}
-		});
-	})
-})
-
-*/
-function initConnect(cb){
-	/*
-	getCurrentTab(function(tab){
-		if(port){
-			port.disconnect();
-			// port = undefined;
+chrome.runtime.onMessage.addListener(function(rq, send, response) {
+	if (server.lock) {
+		if(send.tab.id != server.targetid) return;
 		}
+	if (rq.cmd == "reporthref" && server.ucallback) {
+		// console.log(server);
+		if (!server.lock) {
+			// server.targetid = send.tab.id;
 
-		port = chrome.tabs.connect(tab.id, {});
-
-		port.onMessage.addListener(function (msg) {
-			// console.log('background.js' + msg);
-			if (msg.cmd == "url") {
-				cacheurls.push(msg.url);
-				if (getUrlsCallBack) {
-					getUrlsCallBack(msg.url);
-				}
-			}else if(msg.cmd == 'analyze'){
-				if (namelistCallBack) {
-					namelistCallBack(msg.namelist);
-				}
-			}
-		});
-		
-		if(cb) cb();
-	})
-	*/
-	if(cb)cb();
-}
-
-function getTabUrls( cb){
-	cacheurls = [];
-	getUrlsCallBack = cb;
-	// console.log(port);
-	port.postMessage({cmd:'geturl'});
-}
-
-function analyze(url, cb){
-	// console.log('analyze' + url);
-	namelistCallBack = cb;
-	port.postMessage({cmd:'analyze', url:url, row:cacheData.currentRow});
-}
-
-function highlight(show, url, name){
-	port.postMessage({cmd:'highlight', url:url, name:name, show:show});
-}
-
-// 读取本地excel文件
-function readWorkbookFromLocalFile(file, callback) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        var data = e.target.result;
-        cacheData.workbook = XLSX.read(data, { type: 'binary' });
-        if(callback) callback(cacheData.workbook.SheetNames);
-    };
-    reader.readAsBinaryString(file);
-}
-
-function getSheetNames(){
-	if(cacheData.workbook)
-		return cacheData.workbook.SheetNames;
-}
-
-function preViewSheet(sheetName){
-	if(!cacheData.workbook){
-		console.log('no valid workbook data');
+			server.urls.push({
+				href: rq.href,
+				frameId: send.frameId,
+				tabid: send.tab.id,
+				tabtitle: send.tab.title
+			});
+			server.ucallback(send.tab.id, rq.href);
+		}
+		response('return');
 		return;
 	}
-	// console.log(sheetName);
-
-	var worksheet = cacheData.workbook.Sheets[sheetName];
-    var xlsdata = '<html><body><table>';
-    var samples = XLSX.utils.sheet_to_csv(worksheet)
-	xlsdata += csv2table(samples);
-	xlsdata += '</table></body></html>';
-
-	// console.log(xlsdata);
-	// xlsdata = "alert('ok');"
-	chrome.tabs.create({url:chrome.extension.getURL('assets/scripts/preview.html')}, function(tab){
-		// console.log(tab);
-		chrome.tabs.onUpdated.addListener(function(tid, cinfo, t_tab){
-			if(tab.id = tid && cinfo.status == 'complete'){
-				chrome.tabs.sendMessage(tid, {"showtable": xlsdata});
-			}
-		})
+	if (rq.cmd == "querydata") {
+		// console.log('querydata');
+		var worksheet = server.workbook.Sheets[server.sheet];
+		var samples = XLSX.utils.sheet_to_csv(worksheet)
+		var xlsdata = csv2array(samples, server.row);
 		
-		// chrome.tabs.executeScript(tab.id, {code:xlsdata, runAt:"document_end"});
-	});
+		response({match:server.match, data:xlsdata, row:server.row});
+		return;
+	}
+	if(rq.cmd == "prev"){
+		server.row -= server.row > 0?1:0;
+		response();
+		return;
+	}
+	if(rq.cmd == "next"){
+		var sheet = server.workbook.Sheets[server.sheet];
+		var range = XLSX.utils.decode_range(sheet['!ref']);
+		if(range)
+				server.row += range.e.r > server.row?1:0;
+		response();
+		return;
+	}
+	if(rq.cmd == "binding"){
+		server.match[rq.objName] = rq.index;
+	}
+
+});
+
+function csv2array(data, rowIndex) {
+	// 将csv转换成表格
+	var rows = data.split('\n');
+	rows.pop(); // 最后一行没用的
+	var columns = rows[rowIndex].split(',');
+	return columns;
 }
-// 将csv转换成表格
-function csv2table(csv) {
-    var html = '';
-    var rows = csv.split('\n');
-    rows.pop(); // 最后一行没用的
-    var index = 0;
-    rows.forEach(function (row, idx) {
-        
-        var columns = row.split(',');
-        columns.unshift(idx + 1); // 添加行索引
-        if (idx == 0) { // 添加列索引
-            html += '<tr>';
-            for (var i = 0; i < columns.length; i++) {
-                html += '<th>' + (i == 0 ? '' : String.fromCharCode(65 + i - 1)) + '</th>';
-            }
-            html += '</tr>';
-        }
-        html += '<tr data="' + idx + '">';
-        columns.forEach(function (column) {
-            html += '<td>' + column + '</td>';
-        });
-        html += '</tr>';
-        index ++;
-        
-    });
-    html += '';
-    return html;
-}
-
-
-function getXlsCell(columnName){
-	if(!cacheData.workbook) return;
-
-	var worksheet = cacheData.workbook.Sheets[cacheData.select];
-	// console.log(worksheet);
-	if(!worksheet) return;
-	var cellName = columnName + cacheData.currentRow;
-	// console.log(cellName);
-	var v = (worksheet[cellName] || {v:undefined}).v;
-	// console.log(v);
-	return v;
+// 读取本地excel文件
+function readWorkbookFromLocalFile(file, readDone) {
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		var data = e.target.result;
+		server.workbook = XLSX.read(data, {
+			type: 'binary'
+		});
+		server.sheet = server.workbook.SheetNames[0];
+		if (readDone) readDone(server.filename);
+	};
+	server.filename = file.name;
+	reader.readAsBinaryString(file);
 }
 
-function fillData(ctrlId, value){
-	port.postMessage({cmd:'filldata', id:ctrlId, value:value, url:cacheData.url});
+function getSheetNames() {
+	if (server.workbook)
+		return server.workbook.SheetNames;
+	return [];
 }
+
+function selectSheet(nameOfSheet) {
+	server.sheet = nameOfSheet;
+}
+
--- assets/scripts/inject.js
+++ assets/scripts/inject.js
@@ -1,86 +1,285 @@
+var server = {
+	extid: "",
+	send: function(obj, response) {
+		chrome.runtime.sendMessage( obj, function(rp) {
+			if (response) response(rp);
+		});
+	}
+}
+var service = {
+	match:{},
+	cache:undefined,
+	reporthref: function() {
+		server.send({
+			cmd: "reporthref",
+			href: window.location.href
+		});
+	},
+	querydata: function( list) {
+		server.send({
+			cmd: "querydata"
+		}, function(data){
+			if(data == undefined) return;
+			if(list) list(data);
+			this.match = data.match;
+			this.cache = data.data;
+		});
+	},
+	prev:function(done){
+		server.send({
+			cmd:'prev'
+		}, done);
+	},
+	next:function(done){
+		server.send({
+			cmd:'next'
+		}, done);
+	},
+	binding:function(index,objName){
+		// save to localstorage
+		
+		
+		server.send({
+			cmd:'binding',
+			objName:objName,
+			index:index
+		});
+		
+		
+	},
+	getIndex:function(objName, done){
+		server.send({
+			cmd:'getIndex',
+			objName:objName
+		},done)
+	}
+}
 
+var client = {
+	isactived: false,
+	query: function(rq, sender) {
+		server.extid = sender.id;
+		service.reporthref();
+	},
+	active: function(rq, sender) {
+		// console.log(rq);
+		if (this.isactived) return;
+		this.isactived = true;
+		activeOperation();
+		
+	}
+}
 
-(function(){
-    // console.log('inject');
-    var conn ;
-    chrome.runtime.onConnect.addListener(function(port){
-        console.log('onConnect');
-        
-        port.onMessage.addListener(function(msg){
-            
-            if(msg.cmd == 'geturl'){
-                port.postMessage({cmd:'url', url:window.location.href});
-            }else if(msg.cmd == 'analyze' && msg.url == window.location.href){
-                var idList = [];
-                document.querySelectorAll('form [name]').forEach(function(obj){
-                    idList.push(obj.name);
-                });
-                port.postMessage({cmd:'analyze', namelist:idList,url:window.location.href});
-                addFlowControl(msg.row);
-            } else if (msg.cmd == 'highlight' && msg.url == window.location.href) {
-                console.log("hight");
+function checkIsNeedShowWindow(){
+	service.querydata(function(data){
+		// console.log(data);
+		var isNeed = false;
+		for(var k in data.match){
+			// 检查是否存在特定的控件名称
+			// console.log(k);
+			var obj = $("[name='" + k + "']");
+			// console.log(obj)
+			if(obj.length){
+				isNeed = true;
+				break;
+			}
+		}
+		if(!isNeed) return;
+		
+		addFlowControl(data.row);
+	});
+}
 
-                $("#highlightControl").remove();
-                if (!msg.show) return;
-
-                var obj = document.querySelector("[name='" + msg.name + "']");
-                obj.scrollIntoView();
-
-                console.log(obj.Width);
-                var pos = { top: $(obj).offset().top, left: $(obj).offset().left };
-
-                var cover =
-                    "<div style='position:absolute; background-color:black;color:yellow;valign:middle;text-align:center;" +
-                    "width:" + obj.offsetWidth + "px;" + "height: " + obj.offsetHeight + "px;" +
-                    "' id='highlightControl'></div>";
-                console.log(cover);
-                cover = $(cover);
-                cover.text(msg.name);
-
-                $("body").append(cover);
-
-                cover.offset({ top: pos.top, left: pos.left });
-
-
-
-            } else if (msg.cmd == 'filldata' && msg.url == window.location.href) {
-                //console.log(msg);
-                var xpath = "[name='" + msg.id + "']";
-                // console.log(xpath);
-                var obj = document.querySelector(xpath);
-                var realVal = msg.value;
-                if(obj.type == "select-one"){
-                    realVal = $(obj).find("option:contains('" + msg.value + "')").val() || msg.value;
-                }
-                $(obj).val(realVal);
-            } 
-        });
-        
-    });
-    chrome.runtime.sendMessage({cmd:"active", url:window.location.href}, function(response){
-        if(response.url != window.location.href) return;
-        addFlowControl(response.row);
-    });
-    function addFlowControl(row){
+function addFlowControl(row){
 
         var div = document.createElement("div");
         div.id = "flow";
         div.innerHTML = "批量数据填表助手 行号:<span id='agency_row'></span><br><button id='agency_prev'>上一条</button><button id='agency_next'>下一条</button>";
         div.style = "box-shadow: 2px 2px 5px #888888;color:white;padding:10px;valign:middle;border-radius: 6px;position:fixed;bottom: 1rem;left: 1rem;z-index: 9999;background-color: darkblue;"
         document.body.appendChild(div);
+		
+		$("#agency_row").text(row);
 
-        $("#agency_row").text(row);
-
-        $("#agency_prev").click(function () {
-            chrome.runtime.sendMessage({ cmd: 'prev', url: window.location.href }, function(response){
-                $("#agency_row").text(response.row);
-            });
+        $("#agency_prev").click(function () {            
+            	service.prev(function(){
+            		fillData();
+            	})          
         })
         $("#agency_next").click(function () {
-            chrome.runtime.sendMessage({ cmd: 'next', url: window.location.href }, function(response){
-                $("#agency_row").text(response.row);
-            });
+            service.next(function(){
+            	fillData();
+            }) 
         })
+		
     }
 
-})();
+function fillData(){
+	service.querydata(function(data){
+		if(data == undefined) return;
+		$("#agency_row").text(data.row);
+		// console.log(data);
+		for(var k in data.match){
+			// console.log(k)
+			var index = data.match[k];
+			// console.log(index)
+			var val = data.data[index];
+			var obj = $("[name='" + k + "']")[0];
+			if(obj == undefined) continue
+			// console.log()
+			if(obj.type == "select-one"){
+				val = $(obj).find("option:contains('" + val + "')").val() || val;
+				// console.log(val);
+			}
+			$(obj).val(val);
+		}
+	})
+}
+function setMenuData(e, data, match){
+	// console.log(e);
+	var objFor = $(e.target).attr('for');
+	// console.log(objFor);
+	
+	var showtable = $("<table style='border-collapse: collapse;'></table>");
+	var ch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var columnName = ch.split('');
+	
+	var index = match[objFor];
+	for (var k in data) {
+		
+		var radio = "<input type='radio' name='datatarget' colid='" + k + "' for='" + objFor + "' " + (k == index?"checked":"")+ "></input>";
+		$(showtable).append($("<tr><td style='border:1px solid black;'>" 
+		+ columnName[k] 
+		+ "</td><td  style='border:1px solid black;'>" 
+		+ radio + "</td><td style='border:1px solid black;' columnid='" 
+		+ k + "'>"
+		+ data[k] + "</td></tr>"));
+	}
+	var recordBtn = $("<button id='prev'>上一条</button><button id='next'>下一条</button>");
+	var flowDiv = $("<div style='position:absolute;background:gainsboro;border:solid 1px darkgrey;width:300px;height:auto;'></div>")
+	.append(recordBtn)
+	.append(showtable);
+	
+	e = e || window.event;
+	flowDiv.css("left", e.clientX);
+	flowDiv.css("top", e.clientY);
+	flowDiv.attr("id", "datamenu");
+	$("body").append(flowDiv);
+	
+	$("#prev").click(function(e){
+		service.prev(function(){
+			service.querydata(function(data){
+				for(var k in data.data){
+					$("[columnid='" + k + "']").text(data.data[k]);
+				}
+			})
+		})
+	});
+	$("#next").click(function(e){		
+		service.next(function(){			
+			service.querydata(function(data){				
+				for(var k in data.data){
+					$("[columnid='" + k + "']").text(data.data[k]);
+				}
+			})
+		})
+	});
+	$("input[type='radio'][name='datatarget']").click(function(e){
+		if(!$(this).is(":checked")){
+			return;
+		}
+		// save relationship
+		// console.log(objFor);
+		var index = $(this).attr("colid");
+		service.binding(index, objFor);
+		var storage = window.localStorage;
+		storage.setItem(objFor, index);
+	});
+}
+function showXlsData(e) {
+	console.log('showxls');
+	// var forObjName = $(e.srcElement).attr("for");
+	
+	service.querydata(function(data) {
+		console.log(data)
+		setMenuData(e, data.data, data.match);		
+	})
+	
+	
+}
+
+function activeOperation() {
+	$(document).mouseup(function(e) {
+		if(e.target.id == "datamenu") return;
+		if($(e.target).parents("#datamenu").length > 0) return;
+		
+		// 绑定数据
+		$("#datamenu").remove();
+		// console.log(document.activeElement.tagName);
+		// 除了body之外的都是操作对象
+		if (document.activeElement.tagName == "BODY"  ) {
+			// console.log(e);
+			
+			return;
+		}
+		if (document.activeElement.type == "submit") return;
+		if (document.activeElement.type == "button") return;
+		if (document.activeElement.tagName == "button") return;
+		var obj = document.activeElement;
+		var objName = obj.name;
+		
+		if(!$('[for="' + objName + '"]')) return;
+		
+		// 显示操作窗口
+		var pos = {
+			top: $(obj).offset().top + 2,
+			left: $(obj).offset().left + obj.offsetWidth + 3
+		};
+
+		var cover =
+			"<div style='position:absolute; background-color:red;color:yellow;valign:middle;text-align:center;border-radius: 3px;" +
+			"width:16" + /*obj.offsetWidth + */ "px;" + "height: " + (obj.offsetHeight - 4) + "px;" +
+			"' id='highlightControl' for='" + objName + "' title='点击进行配置'></div>";
+		// console.log(cover);
+		cover = $(cover);
+		cover.text('>');
+		
+		$("body").append(cover);
+
+		cover.offset({
+			top: pos.top,
+			left: pos.left
+		});
+		
+		cover.click(function(e) {
+			e = e || window.event;
+			showXlsData(e);
+			return false;
+		});
+	});
+
+
+}
+
+function loadPattern(){
+	var storage = window.localStorage;
+	for(var i = 0;i<storage.length;i++){
+		var key = storage.key(i)
+		if($("[name='" + key +"']")){
+			var val = storage.getItem(key);
+			service.match[key] = val;
+			service.binding(val, key);
+		}
+	}
+}
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (client[request.cmd] != undefined) client[request.cmd](request, sender);
+		sendResponse();
+	}
+);
+
+loadPattern();
+// console.log(service.match)
+checkIsNeedShowWindow();
+fillData();
